82088209c9133226dc57b663776f0a709a6ff64e
BaldwinHe <heyanbai1999@gmail.com>
2018-10-19 21:12:40 +0800

    Solve Moving Problem
    Image/Parsing can move with the same pace of mouse.

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ ori_parsing = cv2.cvtColor(ori_parsing, cv2.COLOR_BGR2GRAY)
 
 ix = -1
 iy = -1
+temp_img = -1
+temp_parsing = -1
 dst = image
 dst_img = image
 parsing = ori_parsing / 255.0
@@ -16,24 +18,27 @@ dst_parsing = parsing
 parsing = np.expand_dims(parsing,2)
 ori_parsing = np.expand_dims(ori_parsing,2)
 ori_255 = (dst_parsing * 255).astype("uint8")
+flag = False
 
 
 def move(event,x,y, flags, param):
-    global dst,dst_parsing,parsing,image,dst_img,ix,iy,ori_255
+    global dst,dst_parsing,parsing,image,dst_img,ix,iy,ori_255,temp_img,temp_parsing
     if event == cv2.EVENT_LBUTTONDOWN:
         print(event,end=" ")
         print(flags,end="\n")
         ix, iy = x, y
+        temp_parsing,temp_img = dst_parsing,dst_img
     elif event == cv2.EVENT_MOUSEMOVE and flags == cv2.EVENT_FLAG_LBUTTON and ori_255[iy,ix] != 0:
         print(event, end=" ")
         print(ori_255[iy,ix],end =" ")
         print(flags, end="\n")
+        flag = True
         rows, cols, _ = parsing.shape
         dx = x - ix
         dy = y - iy
         M = np.float32([[1, 0, dx], [0, 1, dy]])
-        dst_parsing = cv2.warpAffine(dst_parsing, M, (cols, rows))
-        dst_img = cv2.warpAffine(dst_img, M, (cols, rows))
+        dst_parsing = cv2.warpAffine(temp_parsing, M, (cols, rows))
+        dst_img = cv2.warpAffine(temp_img, M, (cols, rows))
 
         dst_parsing = np.expand_dims(dst_parsing, 2)
         cv2.imshow('img', dst_img)
